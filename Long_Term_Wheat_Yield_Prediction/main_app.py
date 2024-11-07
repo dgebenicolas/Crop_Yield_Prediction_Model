2c3341b8f0df590d736385b0a5a8ea0240d7fdc9
Dgebe N <Dgebe N@oagr.olzhaagro.kz>
2024-11-07 08:50:33 +0500

    g

--- Long_Term_Wheat_Yield_Prediction/main_app.py
+++ Long_Term_Wheat_Yield_Prediction/main_app.py
@@ -109,6 +109,7 @@ def main():
             if has_yield:
                 id_columns, process_df = process_data_yield(result)
                 result_df, error = predict_yields(id_columns, process_df, current_dir,  model, prep_path, model_type)
+                st.write(result.columns)
             else:
                 id_columns, process_df = process_data(result)
                 result_df, error = predict_yields(id_columns, process_df, current_dir,  model, prep_path, model_type)
